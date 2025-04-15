99e2053058e196a4e7f4ce044cc4a035b2e212a7
sh5080 <prpn97@gmail.com>
2025-04-15 16:09:29 +0900

    feat: 유효성검증

--- go.mod
+++ go.mod
@@ -27,6 +27,10 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.19 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -34,7 +38,10 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
-	golang.org/x/crypto v0.20.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
---
+++ pkg/utils/validator.util.go
@@ -0,0 +1,64 @@
+// pkg/utils/validator.go
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+	
+	// 구조체 필드 이름 대신 json 태그 이름 사용
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return fld.Name
+		}
+		return name
+	})
+}
+
+// Validate는 구조체의 유효성을 검증합니다.
+func Validate(data interface{}) error {
+	if data == nil {
+		return fmt.Errorf("데이터가 nil입니다")
+	}
+
+	err := validate.Struct(data)
+	if err == nil {
+		return nil
+	}
+
+	// 검증 오류 메시지 포맷팅
+	validationErrors, ok := err.(validator.ValidationErrors)
+	if !ok {
+		return err
+	}
+
+	errorMessages := make([]string, 0, len(validationErrors))
+	for _, e := range validationErrors {
+		switch e.Tag() {
+		case "required":
+			errorMessages = append(errorMessages, fmt.Sprintf("%s 필드는 필수입니다", e.Field()))
+		case "oneof":
+			errorMessages = append(errorMessages, fmt.Sprintf("%s 필드는 %s 중 하나여야 합니다", e.Field(), e.Param()))
+		case "required_if":
+			params := strings.Split(e.Param(), " ")
+			if len(params) >= 2 {
+				errorMessages = append(errorMessages, fmt.Sprintf("%s 필드는 %s가 %s일 때 필수입니다", e.Field(), params[0], params[1]))
+			} else {
+				errorMessages = append(errorMessages, fmt.Sprintf("%s 필드는 조건에 따라 필수입니다", e.Field()))
+			}
+		default:
+			errorMessages = append(errorMessages, fmt.Sprintf("%s 필드가 %s 규칙을 만족하지 않습니다", e.Field(), e.Tag()))
+		}
+	}
+
+	return fmt.Errorf(strings.Join(errorMessages, ", "))
+}
