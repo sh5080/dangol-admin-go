b936d21b990f5ec37ca29899c997937cd948e001
sh5080 <prpn97@gmail.com>
2025-04-16 16:38:36 +0900

    fix: neondb sni golang호환 sslmode=verify-full 로 접근해야 해결됨

--- pkg/configs/db.config.go
+++ pkg/configs/db.config.go
@@ -2,22 +2,16 @@ package config
 
 // DBConfig는 데이터베이스 연결 설정을 위한 구조체입니다.
 type DBConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
+	DatabaseURL string
 }
 
 // NewDBConfig는 환경 변수에서 데이터베이스 설정을 로드합니다.
 func (c *Config) NewDBConfig() *DBConfig {
-	return &DBConfig{
-		Host:     GetEnvOrDefault("DB_HOST", "localhost"),
-		Port:     GetEnvOrDefault("DB_PORT", "5432"),
-		User:     GetEnvOrDefault("DB_USER", "postgres"),
-		Password: GetEnvOrDefault("DB_PASSWORD", "postgres"),
-		Database: GetEnvOrDefault("DB_NAME", "postgresdb"),
-		SSLMode:  GetEnvOrDefault("DB_SSL_MODE", "disable"),
+	databaseURL := GetEnvOrDefault("DATABASE_URL", "postgresql://neondb_owner:npg_et5nvG0lMogX@ep-nameless-snowflake-a15r97wt-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=verify-full")
+	if databaseURL != "" {
+		return &DBConfig{
+			DatabaseURL: databaseURL,
+		}
 	}
-} 
+	return nil
+}
