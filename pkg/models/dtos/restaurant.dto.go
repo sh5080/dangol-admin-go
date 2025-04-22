1e9549037b8ed09db0d654bfceadc46e94c969a5
sh5080 <prpn97@gmail.com>
2025-04-22 16:53:40 +0900

    feat: restaurantRequestQuery

---
+++ pkg/models/dtos/restaurant.dto.go
@@ -0,0 +1,10 @@
+package dtos
+
+import "lambda-go/pkg/models"
+
+// RestaurantRequestQuery는 매장 요청 조회를 위한 쿼리 파라미터 DTO입니다.
+type RestaurantRequestQuery struct {
+	Page     int                   `json:"page"`
+	PageSize int                   `json:"pageSize"`
+	Status   *models.RequestStatus `json:"status,omitempty"`
+}
